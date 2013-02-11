c244e5de05839a759c48073f054ca884dfd7959a
Jacob Hamacher <jacob@hamacher.se>
2013-02-11 18:08:26 +0100

    Added area for Squeezebox

---
+++ Areas/squeezebox/squeezeboxAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace hydra.Areas.squeezebox
+{
+    public class squeezeboxAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "squeezebox";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "squeezebox_default",
+                "squeezebox/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
