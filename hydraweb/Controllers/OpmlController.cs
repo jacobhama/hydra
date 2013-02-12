34738dbd5dd43105d140f06e44aa03f00b5db9ca
Jacob Hamacher <jacob@hamacher.se>
2013-02-12 10:46:46 +0100

    First working opml categorylist

---
+++ hydraweb/Controllers/OpmlController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestSharp;
+using hydra.Models;
+
+namespace hydra.Controllers
+{
+    public class OpmlController : Controller
+    {
+        //
+        // GET: /Opml/
+
+        public ActionResult Index()
+        {
+            //Should return a list of all categories in opml format
+            var srApi = new SrApi();
+            var programCategories = srApi.GetProgramCategories();
+
+            return View(programCategories);
+        }
+
+    }
+}
---
+++ hydraweb/Models/SrApi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+
+namespace hydra.Models
+{
+    public class SrApi
+    {
+        const string BaseUrl = "http://api.sr.se/api/v2";
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = BaseUrl;
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+            return response.Data;
+        }
+
+        public List<ProgramCategory> GetProgramCategories()
+        {
+            var request = new RestRequest();
+            request.Resource = "/programcategories/?pagination=false";
+            request.RootElement = "programcategories";
+            return Execute<List<ProgramCategory>>(request);
+        }
+    }
+
+    public class ProgramCategory
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
